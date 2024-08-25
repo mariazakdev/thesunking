2c37a17bc7e379f5b59b42d49d010bebf9b132d5
Maria Zak <mzakart@gmail.com>
2024-08-25 07:05:25 -0400

    color

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -18,11 +18,11 @@ const Nav = () => {
           {isOpen ? 'X' : 'Menu'}
         </button>
         <div className={`nav-links ${isOpen ? 'show' : ''}`}>
-          <Link to="home" smooth={true} offset={-headerHeight} duration={500} onClick={handleToggle}>Home</Link>
-          <Link to="about" smooth={true} offset={-headerHeight} duration={500} onClick={handleToggle}>About Us</Link>
-          <Link to="gallery" smooth={true} offset={-headerHeight} duration={500} onClick={handleToggle}>Gallery</Link>
-          <Link to="contact" smooth={true} offset={-headerHeight} duration={500} onClick={handleToggle}>Contact Us</Link>
-          <Link to="support" smooth={true} offset={-headerHeight} duration={500} onClick={handleToggle}>Support Us</Link>
+          <Link to="home" className='link' smooth={true} offset={-headerHeight} duration={500} onClick={handleToggle}>Home</Link>
+          <Link to="about" className='link' smooth={true} offset={-headerHeight} duration={500} onClick={handleToggle}>About</Link>
+          <Link to="gallery" className='link' smooth={true} offset={-headerHeight} duration={500} onClick={handleToggle}>Gallery</Link>
+          <Link to="contact" className='link' smooth={true} offset={-headerHeight} duration={500} onClick={handleToggle}>Contact</Link>
+          <Link to="support" className='link' smooth={true} offset={-headerHeight} duration={500} onClick={handleToggle}>Support</Link>
         </div>
         <div className="social-media-icons">
           {/* Add social media icons here */}
