67067c44dbf72835454708ff9e1cff0fb8e7f69e
Maria Zak <mzakart@gmail.com>
2025-09-14 17:58:04 -0400

    new gallery

--- src/Components/Gallery/Gallery.jsx
+++ src/Components/Gallery/Gallery.jsx
@@ -1,34 +1,4 @@
-// import React from 'react';
 
-// import img1 from '../../Assets/Images/The Sun King 1st Poster.png';
-// import img2 from '../../Assets/Images/Screen Shot 2019-02-18 at 12.46.05 AM.png';
-
-// import './Gallery.scss'; 
-
-// const images = [
-//     img1,
-//     img2,
-
-
-// ];
-
-// const Gallery = () => {
-
-
-//   return (
-//     <div className="gallery">
-//     <div className="gallery-content">
-//       {images.map((image, index) => (
-//         <div key={index} className="gallery-item">
-//           <img src={image} alt={`Gallery ${index}`} />
-//         </div>
-//       ))}
-//     </div>
-//     </div>
-//   );
-// }
-
-// export default Gallery;
 import React, { useState } from 'react';
 
 import img1 from '../../Assets/Images/The Sun King 1st Poster.png';
@@ -50,29 +20,52 @@ import img14 from '../../Assets/Images/Screenshot 2025-08-03 at 8.01.22 PM.png
 
 import './Gallery.scss';
 
+// const images = [
+//   img1, img2, img3, img4,
+//    img5, img6, img7, img1,img8, img9 
+//    ,img10, img11, img12, img13, img14
+//   ];
+
+// const Gallery = () => {
+//   const [enlargedIndex, setEnlargedIndex] = useState(null);
+
+//   const handleImageClick = (index) => {
+//     setEnlargedIndex(enlargedIndex === index ? null : index);
+//   };
+
+//   return (
+//     <div className="gallery">
+//       <h3>Past Productions</h3>
+//       <div className="gallery-content">
+//         {images.map((image, index) => (
+//           <div
+//             key={index}
+//             className={`gallery-item ${enlargedIndex === index ? 'enlarged' : ''}`}
+//             onClick={() => handleImageClick(index)}
+//           >
+//             <img src={image} alt={`Gallery ${index}`} />
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Gallery;
 const images = [
   img1, img2, img3, img4,
-   img5, img6, img7, img1,img8, img9 
-   ,img10, img11, img12, img13, img14
-  ];
+  img5, img6, img7, img8, 
+  img9, img10, img11, img12, 
+  img13, img14
+];
 
 const Gallery = () => {
-  const [enlargedIndex, setEnlargedIndex] = useState(null);
-
-  const handleImageClick = (index) => {
-    setEnlargedIndex(enlargedIndex === index ? null : index);
-  };
-
   return (
     <div className="gallery">
       <h3>Past Productions</h3>
       <div className="gallery-content">
         {images.map((image, index) => (
-          <div
-            key={index}
-            className={`gallery-item ${enlargedIndex === index ? 'enlarged' : ''}`}
-            onClick={() => handleImageClick(index)}
-          >
+          <div key={index} className="gallery-item">
             <img src={image} alt={`Gallery ${index}`} />
           </div>
         ))}
