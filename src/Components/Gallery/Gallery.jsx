81a2932fc80176820c74d7789bd7e7ce43bdbbe9
maria <mzakart@gmail.com>
2026-05-30 14:50:10 -0400

    Update email. Gallery clean up

--- src/Components/Gallery/Gallery.jsx
+++ src/Components/Gallery/Gallery.jsx
@@ -1,6 +1,4 @@
 
-import React, { useState } from 'react';
-
 import img1 from '../../Assets/Images/The Sun King 1st Poster.png';
 import img2 from '../../Assets/Images/Screen Shot 2019-02-18 at 12.46.05 AM.png';
 import img3 from '../../Assets/Images/Screenshot 2025-07-16 at 1.07.29 PM.png';
@@ -17,41 +15,8 @@ import img13 from '../../Assets/Images/Screenshot 2025-07-24 at 2.36.01 PM.png
 import img14 from '../../Assets/Images/Screenshot 2025-08-03 at 8.01.22 PM.png';
 
 
-
 import './Gallery.scss';
 
-// const images = [
-//   img1, img2, img3, img4,
-//    img5, img6, img7, img1,img8, img9 
-//    ,img10, img11, img12, img13, img14
-//   ];
-
-// const Gallery = () => {
-//   const [enlargedIndex, setEnlargedIndex] = useState(null);
-
-//   const handleImageClick = (index) => {
-//     setEnlargedIndex(enlargedIndex === index ? null : index);
-//   };
-
-//   return (
-//     <div className="gallery">
-//       <h3>Past Productions</h3>
-//       <div className="gallery-content">
-//         {images.map((image, index) => (
-//           <div
-//             key={index}
-//             className={`gallery-item ${enlargedIndex === index ? 'enlarged' : ''}`}
-//             onClick={() => handleImageClick(index)}
-//           >
-//             <img src={image} alt={`Gallery ${index}`} />
-//           </div>
-//         ))}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Gallery;
 const images = [
   img1, img2, img3, img4,
   img5, img6, img7, img8, 
