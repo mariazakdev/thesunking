6f67c02e35538446f7cf6f12e1c86ea944c6464f
Maria Zak <mzakart@gmail.com>
2026-07-24 09:36:45 -0400

    rehearsal

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -34,6 +34,41 @@
 
 // export default Nav;
 
+// import React, { useState } from 'react';
+// import { Link } from 'react-scroll';
+// import './Nav.scss';
+
+// const Nav = () => {
+//   const [isOpen, setIsOpen] = useState(false);
+
+//   const handleToggle = () => {
+//     setIsOpen(!isOpen);
+//   };
+  
+//   const headerHeight = 100; 
+
+//   return (
+//     <header className="nav">
+//       <nav className={`nav ${isOpen ? 'open' : ''}`}>
+//         <button className="nav-toggle" onClick={handleToggle}>
+//           {isOpen ? 'X' : 'Menu'}
+//         </button>
+//         <div className={`nav-links ${isOpen ? 'show' : ''}`}>
+//           <Link to="home" className='link' smooth={true} offset={-headerHeight} duration={500} onClick={handleToggle}>Home</Link>
+//           <Link to="about" className='link' smooth={true} offset={-headerHeight} duration={500} onClick={handleToggle}>About</Link>
+//           <Link to="gallery" className='link' smooth={true} offset={-headerHeight} duration={500} onClick={handleToggle}>Gallery</Link>
+//           <Link to="contact" className='link' smooth={true} offset={-headerHeight} duration={500} onClick={handleToggle}>Contact</Link>
+//           <Link to="support" className='link' smooth={true} offset={-headerHeight} duration={500} onClick={handleToggle}>Support</Link>
+//         </div>
+//         <div className="social-media-icons">
+//           {/* Add social media icons here */}
+//         </div>
+//       </nav>
+//     </header>
+//   );
+// };
+
+// export default Nav;
 import React, { useState } from 'react';
 import { Link } from 'react-scroll';
 import './Nav.scss';
@@ -75,6 +110,7 @@ const Nav = () => {
         <div className={`nav-links ${isOpen ? 'show' : ''}`}>
           <Link to="home" className='link' smooth={true} offset={-headerHeight} duration={500} onClick={handleToggle}>Home</Link>
           <Link to="about" className='link' smooth={true} offset={-headerHeight} duration={500} onClick={handleToggle}>About</Link>
+          <Link to="rehearsals" className='link' smooth={true} offset={-headerHeight} duration={500} onClick={handleToggle}>Updates</Link>
           <Link to="gallery" className='link' smooth={true} offset={-headerHeight} duration={500} onClick={handleToggle}>Gallery</Link>
           <Link to="contact" className='link' smooth={true} offset={-headerHeight} duration={500} onClick={handleToggle}>Contact</Link>
           <Link to="support" className='link' smooth={true} offset={-headerHeight} duration={500} onClick={handleToggle}>Support</Link>
--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -3,6 +3,7 @@ import About from '../../Components/About/About';
 import Hero from '../../Components/Hero/Hero';
 import ContactPage from '../ContactPage/ContactPage';
 import Gallery from '../../Components/Gallery/Gallery';
+import Rehearsals from '../../Components/Researsals/Rehearsals'; 
 import SupportUs from '../../Components/SupportUs/SupportUs';
 import './HomePage.scss';
 
@@ -11,30 +12,41 @@ export default function HomePage() {
     <div className="homepage">
       <section id="home" className="home" title="Welcome to Our Theatre" modifier="home">
         <Hero />
-     </section>
+      </section>
+
       <section id="about" className="about" title="About Us" modifier="about">
         <About />
       </section>
+
       {/* <section id="team" className="team" title="The Team" modifier="team">
         <h2>Meet The Team</h2>
-      <CrewCast/>
+        <CrewCast/>
       </section> */}
+
       {/* <section id="tickets" className="tickets" title="Tickets" modifier="tickets">
         <h2>Tickets</h2>
         Tickets 
       </section> */}
-      <section id="gallery" className="gallery" title="Gallery" modifier="gallery">
-        <Gallery  />
+
+      <section id="rehearsals" className="rehearsals" title="Rehearsals" modifier="rehearsals">
+        <Rehearsals />
       </section>
+
+      <section id="gallery" className="gallery" title="Gallery" modifier="gallery">
+        <Gallery />
+      </section>
+
       {/* <section id="news" className="news" title="News" modifier="news">
         <h2>News</h2>
         News 
       </section> */}
+
       <section id="contact" className="contact" title="Contact Us" modifier="contact">
-       <ContactPage/>
+        <ContactPage />
       </section>
+
       <section id="support" className="support" title="Support Us" modifier="support">
-      <  SupportUs />
+        <SupportUs />
       </section>
     </div>
   );
